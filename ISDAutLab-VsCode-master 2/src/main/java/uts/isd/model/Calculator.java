42d1080a571082b0e10f95cc20661835a6168c3f
TeraSeo <seotj0413@gmail.com>
2024-04-03 16:43:52 +0900

    3rd April 2024

--- ISDAutLab-VsCode-master 2/src/main/java/uts/isd/model/Calculator.java
+++ ISDAutLab-VsCode-master 2/src/main/java/uts/isd/model/Calculator.java
@@ -2,7 +2,7 @@ package uts.isd.model;
 
 public class Calculator {
     public int add(int a, int b) {
-        return a + b; 
+        return a + b + b; 
     }
 
     public int subtract(int a, int b) {
